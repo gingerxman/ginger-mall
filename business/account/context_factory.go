4148a435a318007c6429b99174aa30bed345ed86
robert <chenru@weizoom.com>
2019-09-17 20:38:18 +0800

    move NewBusinessContext from main.go to account package

--- business/account/context_factory.go
+++ business/account/context_factory.go
@@ -2,19 +2,47 @@ package account
 
 import (
 	"context"
+	"github.com/gingerxman/eel"
 	
 	"github.com/bitly/go-simplejson"
 	
 	"net/http"
 )
 
-var gInstance *ContextFactory
-
-type ContextFactory struct {
-}
-
-//NewContext 构造含有corp的Context
-func (this *ContextFactory) NewContext(ctx context.Context, request *http.Request, userId int, jwtToken string, rawData *simplejson.Json) context.Context {
+func NewBusinessContext(ctx context.Context, request *http.Request, userId int, jwtToken string, rawData *simplejson.Json) context.Context {
+	user := new(User)
+	user.Model = nil
+	user.Id = userId
+	user.RawData = rawData
+	
+	ctx = context.WithValue(ctx, "jwt", jwtToken)
+	user.Ctx = ctx
+	
+	ctx = context.WithValue(ctx, "user", user)
+	
+	//创建corp
+	if rawData != nil {
+		jwtType, _ := rawData.Get("type").Int()
+		if jwtType == 1 {
+			corpId, err := rawData.Get("cid").Int()
+			if err == nil {
+				corp := new(Corp)
+				corp.Model = nil
+				corp.Id = corpId
+				corp.Ctx = ctx
+				
+				//处理corp user
+				corpUserId, _ := rawData.Get("uid").Int()
+				corpUser := NewCorpUserFromId(ctx, corpUserId)
+				corp.CorpUser = corpUser
+				
+				ctx = context.WithValue(ctx, "corp", corp)
+			} else {
+				eel.Logger.Error(err)
+			}
+			
+		}
+	}
 	return ctx
 }
 
--- main.go
+++ main.go
@@ -15,55 +15,15 @@
 package main
 
 import (
-	"context"
-	"net/http"
 	"github.com/gingerxman/eel"
+	"github.com/gingerxman/ginger-mall/business/account"
+	_ "github.com/gingerxman/ginger-mall/middleware"
 	_ "github.com/gingerxman/ginger-mall/models"
 	_ "github.com/gingerxman/ginger-mall/routers"
-	_ "github.com/gingerxman/ginger-mall/middleware"
-	"github.com/bitly/go-simplejson"
-	"github.com/gingerxman/ginger-mall/business/account"
 )
 
-func NewBusinessContext(ctx context.Context, request *http.Request, userId int, jwtToken string, rawData *simplejson.Json) context.Context {
-	user := new(account.User)
-	user.Model = nil
-	user.Id = userId
-	user.RawData = rawData
-	
-	ctx = context.WithValue(ctx, "jwt", jwtToken)
-	user.Ctx = ctx
-	
-	ctx = context.WithValue(ctx, "user", user)
-	
-	//创建corp
-	if rawData != nil {
-		jwtType, _ := rawData.Get("type").Int()
-		if jwtType == 1 {
-			corpId, err := rawData.Get("cid").Int()
-			if err == nil {
-				corp := new(account.Corp)
-				corp.Model = nil
-				corp.Id = corpId
-				corp.Ctx = ctx
-				
-				//处理corp user
-				corpUserId, _ := rawData.Get("uid").Int()
-				corpUser := account.NewCorpUserFromId(ctx, corpUserId)
-				corp.CorpUser = corpUser
-				
-				ctx = context.WithValue(ctx, "corp", corp)
-			} else {
-				eel.Logger.Error(err)
-			}
-			
-		}
-	}
-	return ctx
-}
-
 func main() {
-	eel.Runtime.NewBusinessContext = NewBusinessContext
+	eel.Runtime.NewBusinessContext = account.NewBusinessContext
 	eel.RunService()
 }
 
